c1b653eb3938c802fd8fc9821b29631a6e8b82fd
SHARON DEV <s5678sha@gmail.com>
2020-05-22 13:22:27 +0530

    visualization in htm

---
+++ Retrieving_VisualizingDB/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','ucdavis.edu','uct.ac.za','berkeley.edu','mac.com','columbia.edu','gmail.com','ufp.pt','cam.ac.uk'],
+['2005',57,12,11,14,12,12,13,10,10,7],
+['2006',180,50,47,44,45,44,33,35,35,36]
+];
